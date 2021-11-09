b3e2ccc9d2a00e0d80cea56af03d2966c643cca1
Jerferson <jerfersongriza@gmail.com>
2021-11-09 17:58:47 -0300

    feat(Axios): ensure AxiosHttpClient calls axios.get with correct params

---
+++ src/infra/http/axios-client.ts
@@ -0,0 +1,10 @@
+
+import { HttpGetClient } from '@/infra/http'
+
+import axios from 'axios'
+
+export class AxiosHttpClient {
+  async get (args: HttpGetClient.Params): Promise<void> {
+    await axios.get(args.url, { params: args.params })
+  }
+}
