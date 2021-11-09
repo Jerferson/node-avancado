1c15f5317ee146280dd14431973ccbb1e864bd17
Jerferson <jerfersongriza@gmail.com>
2021-11-09 16:04:03 -0300

    test(FacebookAuth): ensure FacebookApi gets user info

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -16,7 +16,9 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
-    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -37,10 +39,22 @@ describe('FacebookApi', () => {
     await sut.loadUser({ token: 'any_client_token' })
 
     expect(httpClient.get).toHaveBeenCalledWith({
-      url: 'https://graph.facebook.com/oauth/debug_token',
+      url: 'https://graph.facebook.com/debug_token',
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  })
+
+  it('shoul get user info', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        filds: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   })
