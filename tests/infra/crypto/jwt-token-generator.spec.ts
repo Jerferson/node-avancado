d21aa9f8f2e66bada7633ef62bf1e813d434eb6f
Jerferson <jerfersongriza@gmail.com>
2021-11-09 19:14:48 -0300

    refactor(jwt): isolate SUT creation and add faker of data

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -1,16 +1,25 @@
 import { JwtTokenGenetator } from '@/data/contracts/crypto'
+import faker from 'faker'
 
 import jwt from 'jsonwebtoken'
 
 jest.mock('jsonwebtoken')
 
 describe('JwtTokenGenerator', () => {
+  let sut: JwtTokenGenetator
+  let fakeJwt: jest.Mocked<typeof jwt>
+  let secrete: string
+
+  beforeEach(() => {
+    secrete = faker.datatype.uuid()
+    sut = new JwtTokenGenetator(secrete)
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
   it('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenetator('any_secret')
+    const key = faker.random.word()
 
-    await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+    await sut.generateToken({ key, expirationInMs: 1000 })
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secrete, { expiresIn: 1 })
   })
 })
